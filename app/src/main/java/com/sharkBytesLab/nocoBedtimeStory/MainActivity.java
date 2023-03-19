d21e19b9e1c8f0d29719f557c78f6024ffd935aa
shashankmangal10@gmail.com <Sunil@1234>
2023-03-19 13:06:03 +0530

    app version 1 ready

--- app/src/main/java/com/sharkBytesLab/nocoBedtimeStory/MainActivity.java
+++ app/src/main/java/com/sharkBytesLab/nocoBedtimeStory/MainActivity.java
@@ -4,7 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.applovin.sdk.AppLovinSdk;
+import com.applovin.sdk.AppLovinSdkConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppLovinSdk.getInstance( this ).setMediationProvider( "max" );
+        AppLovinSdk.initializeSdk( this, new AppLovinSdk.SdkInitializationListener() {
+            @Override
+            public void onSdkInitialized(final AppLovinSdkConfiguration configuration)
+            {
+                // AppLovin SDK is initialized, start loading ads
+            }
+        } );
+
         //CODE HERE
         String[] titles = getResources().getStringArray(R.array.stories_title);
         String[] contents = getResources().getStringArray(R.array.story_content);
@@ -32,4 +49,23 @@ public class MainActivity extends AppCompatActivity {
         //UPTO HERE
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case  R.id.menu_btn:
+                startActivity(new Intent(MainActivity.this, MenuActivity.class));
+                return true;
+        }
+        return false;
+    }
+
+
 }
